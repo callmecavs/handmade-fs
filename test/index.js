c2f8823c4ab1d044f1be31da5572e61d2d5b7809
Michael Cavalea <callmecavs@gmail.com>
2017-02-27 21:12:06 -0500

    add passing read test

--- test/index.js
+++ test/index.js
@@ -13,18 +13,25 @@ const {
 
 describe('handmade-fs', () => {
   it('should read', done => {
+    const toFiles = './input'
     const toAbs = item => path.join(__dirname, item)
 
     const expected = {
-      [toAbs('./input/top-1.txt')]: 'top 1 content',
-      [toAbs('./input/top-2.txt')]: 'top 2 content',
-      [toAbs('./input/folder/mid-3.txt')]: 'mid 3 content',
-      [toAbs('./input/folder/mid-4.txt')]: 'mid 4 content',
-      [toAbs('./input/folder/folder/bot-5.txt')]: 'bot 1 content'
+      core: {
+        context: __dirname,
+        files: {
+          [toAbs('./input/top-1.txt')]: 'top 1 content\n',
+          [toAbs('./input/top-2.txt')]: 'top 2 content\n',
+          [toAbs('./input/folder/mid-1.txt')]: 'mid 1 content\n',
+          [toAbs('./input/folder/mid-2.txt')]: 'mid 2 content\n',
+          [toAbs('./input/folder/folder/bot-1.txt')]: 'bot 1 content\n'
+        },
+        read: toFiles
+      }
     }
 
     handmade(__dirname)
-      .task(read('./input'))
+      .task(read(toFiles))
       .build()
       .then(result => {
         assert.deepEqual(result, expected)
