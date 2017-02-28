642a8551320c405165714925553ee39266fa44ef
Michael Cavalea <callmecavs@gmail.com>
2017-02-27 21:56:34 -0500

    add write test, abstractions

--- test/index.js
+++ test/index.js
@@ -11,27 +11,53 @@ const {
   write
 } = require('../')
 
+const toAbs = item => path.join(__dirname, item)
+
+const CONTEXT = __dirname
+const TO_INPUT = './input'
+const TO_OUTPUT = './output'
+
+const FILES = {
+  [toAbs('./input/top-1.txt')]: 'top 1 content\n',
+  [toAbs('./input/top-2.txt')]: 'top 2 content\n',
+  [toAbs('./input/folder/mid-1.txt')]: 'mid 1 content\n',
+  [toAbs('./input/folder/mid-2.txt')]: 'mid 2 content\n',
+  [toAbs('./input/folder/folder/bot-1.txt')]: 'bot 1 content\n'
+}
+
 describe('handmade-fs', () => {
   it('should read', done => {
-    const toFiles = './input'
-    const toAbs = item => path.join(__dirname, item)
-
     const expected = {
       core: {
-        context: __dirname,
-        files: {
-          [toAbs('./input/top-1.txt')]: 'top 1 content\n',
-          [toAbs('./input/top-2.txt')]: 'top 2 content\n',
-          [toAbs('./input/folder/mid-1.txt')]: 'mid 1 content\n',
-          [toAbs('./input/folder/mid-2.txt')]: 'mid 2 content\n',
-          [toAbs('./input/folder/folder/bot-1.txt')]: 'bot 1 content\n'
-        },
-        read: toFiles
+        context: CONTEXT,
+        files: FILES,
+        read: TO_INPUT
       }
     }
 
-    handmade(__dirname)
-      .task(read(toFiles))
+    handmade(CONTEXT)
+      .task(read(TO_INPUT))
+      .build()
+      .then(result => {
+        assert.deepEqual(result, expected)
+        done()
+      })
+      .catch(error => console.log(error))
+  })
+
+  it('should write', done => {
+    const expected = {
+      core: {
+        context: CONTEXT,
+        files: FILES,
+        read: TO_INPUT,
+        write: TO_OUTPUT
+      }
+    }
+
+    handmade(CONTEXT)
+      .task(read(TO_INPUT))
+      .task(write(TO_OUTPUT))
       .build()
       .then(result => {
         assert.deepEqual(result, expected)
